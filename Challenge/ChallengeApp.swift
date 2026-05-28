39f16acab42889496d784ba0b590766aad23bbb4
arstanber <arslanberdongar@gmail.com>
2026-05-28 15:49:48 +0500

    Initial Commit

---
+++ Challenge/ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeApp.swift
+//  Challenge
+//
+//  Created by Арслан Бердонгар on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
